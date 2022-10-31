fba9683748365074f1d2f5fd2e2d556f8d7d7934
mldamico <matias.lioneldamico@gmail.com>
2022-10-31 08:56:05 -0300

    Added tab navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,14 +2,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import ChatsScreen from "../screens/ChatsScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ChatScreen from "../screens/ChatScreen";
+import MainTabNavigator from "./MainTabNavigator";
 
 const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Chats" component={ChatsScreen} />
+      <Stack.Navigator
+        screenOptions={{ headerStyle: { backgroundColor: "whitesmoke" } }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={MainTabNavigator}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="Chat" component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
